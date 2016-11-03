74bf8d71c5299544b6e904b0047db004d59670c6
bolvax <bolvax@mail.ru>
2016-11-04 00:18:43 +0300

    Compleate table

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("[data-toggle]").click(function() {
+    var toggle_el = $(this).data("toggle");
+    $(toggle_el).toggleClass("open-sidebar");
+  });
+});
+
+
+// header->
+// section->
+// nav<-
