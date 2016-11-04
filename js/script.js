45b0c27b708c2ec1ebb0e23dd2134631aa970ab8
bolvax <bolvax@mail.ru>
2016-11-04 12:55:50 +0300

    skript + bugfix

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-$(document).ready(function() {
-  $("[data-toggle]").click(function() {
-    var toggle_el = $(this).data("toggle");
-    $(toggle_el).toggleClass("open-sidebar");
-  });
-});
+	var pageWidth = document.documentElement.clientWidth;
+	var pageHeight = document.body.clientHeight;
 
+	nav = document.getElementById('side-panel');
+	nav.style.height = pageHeight-100+'px';
 
-// header->
-// section->
-// nav<-
+    var mainContent = document.getElementById('main'),
+    	nav = document.getElementById('side-panel');
+    	toggleBar = document.getElementById('sidebar-toggle');
+function openNav(){
+    nav.style.width = '240px';
+    mainContent.style.width = (pageWidth-240)+'px';
+    mainContent.style.marginLeft = "240px";
+	toggleBar.onclick = closeNav;
+
+}
+function closeNav(){
+    nav.style.width = '0px';
+    mainContent.style.width = pageWidth+'px';
+    mainContent.style.marginLeft = "0px";
+    toggleBar.onclick = openNav;
+}
+  
