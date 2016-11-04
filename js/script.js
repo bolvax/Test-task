a45d196664a8ade0c72b2ee02ceb6fc9ec984145
bolvax <bolvax@mail.ru>
2016-11-04 23:29:39 +0300

    Final commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 var pageWidth = document.documentElement.clientWidth,
 	pageHeight = document.body.clientHeight,
+	screenHeight = document.documentElement.clientHeight,
 	nav = document.getElementById('side-panel'),
     mainContent = document.getElementById('main'),
     nav = document.getElementById('side-panel'),
     toggleBar = document.getElementById('sidebar-toggle');;
 
 	nav.style.height = pageHeight-100+'px';
-
+	popup.style.height = screenHeight+'px';
 function openNav(){
     nav.style.width = '240px';
     mainContent.style.width = (pageWidth-240)+'px';
@@ -26,7 +27,7 @@ function closePpp(){
 function openPpp(){
     popup.style.display = 'block';
 };
-
+console.log('wdwd');
 document.getElementById("tab-filter").addEventListener("click", openPpp);
 cross.addEventListener("click", closePpp);
 save.addEventListener("click", closePpp);
