920c527d86c9488c0a246b6d95f39779e6b634b4
bolvax <bolvax@mail.ru>
2016-11-17 15:31:12 +0300

    some changeslol

--- js/script.js
+++ js/script.js
@@ -1,33 +1,39 @@
 var pageWidth = document.documentElement.clientWidth,
 	pageHeight = document.body.clientHeight,
 	screenHeight = document.documentElement.clientHeight,
-	nav = document.getElementById('side-panel'),
-    mainContent = document.getElementById('main'),
-    nav = document.getElementById('side-panel'),
-    toggleBar = document.getElementById('sidebar-toggle');;
+	nav = document.getElementById("side-p"),
+    tabFilter = document.getElementById("tab-filter"),
+    mainContent = document.getElementById("main-content"),
+    nav = document.getElementById("side-p"),
+    toggleBar = document.getElementById("sidebar-toggle");
 
-	nav.style.height = pageHeight-100+'px';
-	popup.style.height = screenHeight+'px';
+	nav.style.height = pageHeight+"px";
+	popup.style.height = screenHeight+"px";
+
 function openNav(){
-    nav.style.width = '240px';
-    mainContent.style.width = (pageWidth-240)+'px';
+    nav.style.width = "240px";
+    mainContent.style.width = (pageWidth-240)+"px";
     mainContent.style.marginLeft = "240px";
-	toggleBar.onclick = closeNav;
+    toggleBar.removeEventListener("click", openNav);
+    toggleBar.addEventListener("click", closeNav);
 
 }
 function closeNav(){
-    nav.style.width = '0px';
-    mainContent.style.width = pageWidth+'px';
+    nav.style.width = "0px";
+    mainContent.style.width = pageWidth+"px";
     mainContent.style.marginLeft = "0px";
-    toggleBar.onclick = openNav;
+    toggleBar.removeEventListener("click", closeNav);
+    toggleBar.addEventListener("click", openNav);
 }
 function closePpp(){
-    popup.style.display = 'none';
+    popup.style.display = "none";
 };
 function openPpp(){
-    popup.style.display = 'block';
+    popup.style.display = "block";
 };
-console.log('wdwd');
-document.getElementById("tab-filter").addEventListener("click", openPpp);
+
+toggleBar.addEventListener("click", openNav);
+nav.addEventListener("click", closeNav);
+tabFilter.addEventListener("click", openPpp);
 cross.addEventListener("click", closePpp);
 save.addEventListener("click", closePpp);
